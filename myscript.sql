302581fa8a355228299e0124cc6c703042bd6f53
Paula Tiruma <25-021928@riseba.lv>
2026-02-11 20:54:33 +0200

    myscript.sql

---
+++ myscript.sql
@@ -0,0 +1,10 @@
+create database my_db;
+create schema my_first_schema;
+
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
